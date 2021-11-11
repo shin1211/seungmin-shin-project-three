2e0e7100fc3cb718e2798f50c3ba9963ba6eea63
Seungmin <shin891211@gmail.com>
2021-11-11 14:49:57 -0500

    fixed bugs

--- src/components/currentList/CurrentList.js
+++ src/components/currentList/CurrentList.js
@@ -1,4 +1,6 @@
 import currentListStyle from './CurrentListStyle.module.css';
+import delBtnSvg from '../../image/group-5.svg';
+import completedBtnSvg from '../../image/ellipse-3.svg'
 
 const CurrentList = ({ inputList, completedList, delList, setOpenModal }) => {
 
@@ -9,13 +11,16 @@ const CurrentList = ({ inputList, completedList, delList, setOpenModal }) => {
       setOpenModal(true)
     }
   }
+  const currentDate = new Date().toISOString().split('T')[0];
+
   return (
-    // <section className="current-list-container">
     <div className={currentListStyle["current-list-container"]}>
-      <p className={currentListStyle['current-day']}>2021-11-28</p>
+      <p className={currentListStyle['current-day']}>{currentDate}</p>
       <ul className={currentListStyle["current-list"]}>
         {inputList.length === 0 ? (
-          <li><p className="none-data">Set up goals to start your day!</p></li>
+          <li>
+            <p className="none-data">Set up goals to start your day!</p>
+          </li>
         ) : (
           inputList.map((res) => {
             return (
@@ -24,22 +29,11 @@ const CurrentList = ({ inputList, completedList, delList, setOpenModal }) => {
                 <div className={currentListStyle["btn-container"]}>
                   <button onClick={() => completedList(res.key, res.isCompleted)}>
                     <span className="sr-only">Completed</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16.75" height="16.75" viewBox="0 0 16.75 16.75">
-                      <g id="Ellipse_3" data-name="Ellipse 3" transform="translate(0 16.75) rotate(-90)" fill="none" stroke="#707070" stroke-width="2">
-                        <ellipse cx="8.375" cy="8.375" rx="8.375" ry="8.375" stroke="none" />
-                        <ellipse cx="8.375" cy="8.375" rx="7.375" ry="7.375" fill="none" />
-                      </g>
-                    </svg>
-
+                    <img src={completedBtnSvg} alt="" />
                   </button>
                   <button onClick={() => delList(res.key)}>
                     <span className="sr-only">Delete</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="14.418" height="14.418" viewBox="0 0 14.418 14.418">
-                      <g id="Group_5" data-name="Group 5" transform="translate(-5.795 7.209) rotate(-45)">
-                        <line id="Line_1" data-name="Line 1" y2="18.39" transform="translate(9.195 0)" fill="none" stroke="#707070" stroke-width="2" />
-                        <line id="Line_2" data-name="Line 2" y2="18.39" transform="translate(18.39 9.195) rotate(90)" fill="none" stroke="#707070" stroke-width="2" />
-                      </g>
-                    </svg>
+                    <img src={delBtnSvg} alt="" />
                   </button>
                 </div>
               </li >
@@ -50,7 +44,6 @@ const CurrentList = ({ inputList, completedList, delList, setOpenModal }) => {
       <button onClick={handleShowModal} className={currentListStyle["finish-button"]}>End Day</button>
 
     </div>
-    // </section>⃝
   )
 }
 
--- src/components/userForm/UserForm.js
+++ src/components/userForm/UserForm.js
@@ -1,5 +1,6 @@
-// import UserFormStyle from './UserFormStyle.module.css'
 import userFormStyle from './UserFormStyle.module.css'
+import addBtnSvg from '../../image/group-1.svg'
+
 const UserForm = ({ addingList, userInput, setUserInput }) => {
 
 
@@ -8,10 +9,7 @@ const UserForm = ({ addingList, userInput, setUserInput }) => {
       <label htmlFor="userInput" name="userInput"><span className="sr-only">Provide what your goal for today!</span></label>
       <input id="userInput" className={userFormStyle['user-input']} type="text" value={userInput} onChange={e => setUserInput(e.target.value)} />
       <button className={userFormStyle["form-button"]}>
-        <svg id="Group_1" data-name="Group 1" xmlns="http://www.w3.org/2000/svg" width="16.89" height="16.89" viewBox="0 0 16.89 16.89">
-          <line id="Line_1" data-name="Line 1" y2="16.89" transform="translate(8.445)" fill="none" stroke="#707070" stroke-width="2" />
-          <line id="Line_2" data-name="Line 2" y2="16.89" transform="translate(16.89 8.445) rotate(90)" fill="none" stroke="#707070" stroke-width="2" />
-        </svg>
+        <img src={addBtnSvg} alt="" />
       </button>
     </form>
   )
